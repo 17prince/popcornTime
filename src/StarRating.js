a65c4ff842a8b59bc19b8a8245c10510bfe77e28
Prince <princelalla24@gmail.com>
2024-01-20 11:37:14 +0530

    added propTypes in 'StarRating' component

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -8,6 +9,14 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+};
+
+StarRating.prototype = {
+  maxRating: PropTypes.number.isRequired,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  defaultRating: PropTypes.number,
+  onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
